7bfd56db1c826926959e5dd9c0f32354e2493217
benedekhalaj <bendihalaj@gmail.com>
2022-10-20 14:45:32 +0200

    Add Game class

---
+++ src/main/java/com/codecool/polish_draughts/Game.java
@@ -0,0 +1,21 @@
+package com.codecool.polish_draughts;
+
+public class Game {
+
+    public void start() {
+        // TODO
+    }
+
+    private void playRound() {
+        // TODO
+    }
+
+    private void tryToMakeMove() {
+        // TODO
+    }
+
+    private void checkForWinner() {
+        // TODO
+    }
+
+}
