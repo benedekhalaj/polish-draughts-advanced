39b73045e242dbc728dd90bc59e94aaa66612ea3
benedekhalaj <bendihalaj@gmail.com>
2022-10-20 14:45:24 +0200

    Add Board class

---
+++ src/main/java/com/codecool/polish_draughts/Board.java
@@ -0,0 +1,18 @@
+package com.codecool.polish_draughts;
+
+public class Board {
+
+    public void removePawn(Coordinates position) {
+        // TODO
+    }
+
+    public void movePawn(Coordinates initialPosition, Coordinates newPosition) {
+        // TODO
+    }
+
+    @Override
+    public String toString() {
+        // TODO
+        return "Board{}";
+    }
+}
