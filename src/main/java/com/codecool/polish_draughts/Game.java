bfebd4dfc640815acdae792074c4f58db48ae775
benedekhalaj <bendihalaj@gmail.com>
2022-10-20 16:19:52 +0200

    Add Game player related fields

--- src/main/java/com/codecool/polish_draughts/Game.java
+++ src/main/java/com/codecool/polish_draughts/Game.java
@@ -2,6 +2,11 @@ package com.codecool.polish_draughts;
 
 public class Game {
 
+    private static final char PLAYER_1 = '0';
+    private static final char PLAYER_2 = 'X';
+    private char currentPlayer = PLAYER_1;
+    private boolean isRunning = false;
+
     public void start() {
         // TODO
     }
