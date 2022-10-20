c6c13a0cfaa8246b60ff509bcfbebe8efdad5f5e
benedekhalaj <bendihalaj@gmail.com>
2022-10-20 16:56:53 +0200

    Add length restriction to Board creation

--- src/main/java/com/codecool/polish_draughts/Board.java
+++ src/main/java/com/codecool/polish_draughts/Board.java
@@ -2,15 +2,20 @@ package com.codecool.polish_draughts;
 
 public class Board {
 
+    private static final int MINIMUM_BOARD_LENGTH = 10;
+    private static final int MAXIMUM_BOARD_LENGTH = 20;
     private Pawn[][] board;
     private int boardLength;
 
     public Board(int boardLength) {
+        if (boardLength < MINIMUM_BOARD_LENGTH || boardLength > MAXIMUM_BOARD_LENGTH) {
+            throw new IllegalArgumentException("Board length cannot be %d, it must be between %d and %d!"
+                    .formatted(boardLength, MINIMUM_BOARD_LENGTH, MAXIMUM_BOARD_LENGTH));
+        }
         this.boardLength = boardLength;
     }
 
     public void removePawn(Coordinates position) {
-        // TODO
     }
 
     public void movePawn(Coordinates initialPosition, Coordinates newPosition) {
